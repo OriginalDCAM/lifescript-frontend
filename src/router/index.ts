8a89934ecc912a80f436ec820eae0661e741c8c6
OriginalDCAM <denzelannette@hotmail.com>
2023-01-24 22:13:28 +0100

    Updated file directory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
     {
       path: '/about',
       name: 'about',
@@ -21,7 +15,19 @@ const router = createRouter({
       path: '/login',
       name: 'login',
 
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../components/Login.vue')
+    },
+    {
+      path: '/',
+      name: 'BookList',
+
+      component: () => import('../components/BookList.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'PageNotFound',
+
+      component: () => import('../components/PageNotFound.vue')
     },
   ]
 })
