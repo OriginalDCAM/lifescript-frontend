1e17184cea68bcd7e86e1afcbee61061318565e8
OriginalDCAM <denzelannette@hotmail.com>
2023-02-01 23:09:54 +0100

    Added a new component for register

---
+++ src/components/Register.vue
@@ -0,0 +1,117 @@
+<template>
+  <label
+    for="FirstName"
+    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+  >
+    <input
+      v-model="firstname"
+      type="text"
+      id="FirstName"
+      placeholder="First name"
+      class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+    />
+
+    <span
+      class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
+    >
+      First name
+    </span>
+  </label>
+  <label
+    for="UserName"
+    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+  >
+    <input
+      v-model="username"
+      type="text"
+      id="UserName"
+      placeholder="Username"
+      class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+    />
+
+    <span
+      class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
+    >
+     Username
+    </span>
+  </label>
+  <label
+    for="UserEmail"
+    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+  >
+    <input
+      v-model="email"
+      type="email"
+      id="UserEmail"
+      placeholder="Email"
+      class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+    />
+
+    <span
+      class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
+    >
+      Email
+    </span>
+  </label>
+
+  <label
+    for="UserPassword"
+    class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+  >
+    <input
+      v-model="password"
+      type="password"
+      id="UserPassword"
+      placeholder="Password"
+      class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+    />
+
+    <span
+      class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
+    >
+      Password
+    </span>
+  </label>
+  <button
+    class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+    @click="register"
+  >
+    Register
+  </button>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      user: [],
+      email: "",
+      firstname: "",
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    register() {
+      axios
+        .post("http://localhost/api/v1/users/", {
+          email: this.email,
+          first_name: this.firstname,
+          username: this.username,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
