13c41a5ccbd8bae91086246f2a9de4df062ac7af
OriginalDCAM <denzelannette@hotmail.com>
2023-02-01 23:09:06 +0100

    Removed some placeholders

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,6 @@
   for="UserEmail"
   class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
 >
-{{ email }}
   <input
     v-model="email"
     type="email"
@@ -22,7 +21,6 @@
   for="UserPassword"
   class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
 >
-{{ password }}
   <input
     v-model="password"
     type="password"
