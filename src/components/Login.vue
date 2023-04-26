d68e0786cc064cc28c7ccf6b196f682b64cfe689
OriginalDCAM <denzelannette@hotmail.com>
2023-04-26 21:33:18 +0200

    Redesigned the user interface of Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,72 +1,47 @@
 <template>
-    <label
-  for="UserEmail"
-  class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
->
-  <input
-    v-model="email"
-    type="email"
-    id="UserEmail"
-    placeholder="Email"
-    class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-  />
-
-  <span
-    class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
-  >
-    Email
-  </span>
-</label>
-<label
-  for="UserPassword"
-  class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
->
-  <input
-    v-model="password"
-    type="password"
-    id="UserPassword"
-    placeholder="Password"
-    class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-  />
-
-  <span
-    class="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
-  >
-    Password
-  </span>
-</label>
-<button class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500" @click="login">Login</button>
-
+  <div class="flex flex-row lg:w-2/4 space-y-4 m-auto overflow-hidden bg-gray-50 border rounded-2xl">
+    <div class="w-1/2 img">
+    </div>
+    <div class="w-1/2 space-y-6 flex flex-col items-center justify-center">
+      <p><span class="text-lg font-bold font-serif">Sign In</span></p>
+      <InputItem v-model="email" type="email" name="Email" />
+      <InputItem v-model="password" type="password" name="Password" />
+      <button
+        class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+        @click="login" type="submit">Login</button>
+      <span class="text-teal-600">{{ message }}</span>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import axios from 'axios'
+import { ref } from 'vue'
+import InputItem from '@/components/InputItem.vue';
 
-export default {
-    name: 'Login',
-    data() {
-        return {
-            user: [],
-            email: '',
-            password: ''
-        }
-    },
-    methods: {
-        login() {
-            axios.post('http://localhost/api/v1/users/login', {
-                email: this.email,
-                password: this.password
-            })
-            .then((response) => {
-                console.log(response)
-            }).catch((error) => {
-                console.log(error)
-            });
-        }
-    }
+let message = ref();
+let email = ref();
+let password = ref();
+
+function login() {
+  axios.post('http://localhost/api/v1/users/login', {
+    email: email.value,
+    password: password.value
+  })
+    .then((response) => {
+      message = response.data;
+    }).catch((error) => {
+      console.log(error.data);
+    });
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+.img {
+  background-image: url("https://images.unsplash.com/photo-1556566952-11eff3d06ed4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80");
+  background-repeat:no-repeat;
+  background-size:contain;
+  height: 40rem;
+  width: 50%;
+}
 </style>
