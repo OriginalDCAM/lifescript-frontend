6350c6d003682438980a44acfc4f1accaa6c48b3
OriginalDCAM <denzelannette@hotmail.com>
2023-05-16 23:16:47 +0200

    Added a reusable component for input fields

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,37 +1,38 @@
 <template>
     <div>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-        <button class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500" @click="getBooks">Load Books</button>
+        <p class="text-lg text-emerald-500" v-for="book in books">
+            {{ book.first_name }}
+            {{ book.username }}
+        </p>
+        <button
+            class="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+            @click="getBooks">Load Books</button>
     </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import axios from 'axios'
 
 
-    export default {
-        name: 'BookList',
-        data() {
-            return {
-                books: [],
-            }
-        },
-        methods: {
-            getBooks() {
-                axios.get('http://localhost/api/v1/users/?skip=0&limit=100')
-                    .then((response) =>  {
-                        console.log(response.data)
-                    })
-                    .catch((error) => {
-                        console.log(error.data)
-                    })
-            }
+export default {
+    name: 'BookList',
+    data() {
+        return {
+            books: [],
+        }
+    },
+    methods: {
+        getBooks() {
+            axios.get('http://localhost/api/v1/users/?skip=0&limit=100')
+                .then((response) => {
+                    this.books = response.data
+                })
+                .catch((error) => {
+                    console.log(error.data)
+                })
         }
     }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
