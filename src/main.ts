5fd379abd563f96a4cd893b8cb6e9a90a94992db
OriginalDCAM <denzelannette@hotmail.com>
2023-05-19 00:18:16 +0200

    Added Primevue for styling

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+
+// Theme import PrimeVue
+import "primevue/resources/themes/lara-light-teal/theme.css"
+
+// PrimeVue CSS import
+import "primevue/resources/primevue.min.css"
+import 'primeicons/primeicons.css';
+
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(PrimeVue)
 
 app.mount('#app')
